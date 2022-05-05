bf4973ed03782a2c94c0e5e8e57b17e69a7983c7
Kyle KIM <k2wackie@gmail.com>
2022-05-05 22:02:24 +0900

    console.log 제거

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use("/", home);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname + "/../client/build/")));
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(
       path.join(path.join(__dirname + "/../client/build/index.html"))
     );
